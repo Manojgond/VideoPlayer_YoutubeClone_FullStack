44b14b556b0d475fafef10f72fd644120133729c
Manojgond <manojbgond2001@gmail.com>
2024-09-20 21:44:14 +0530

    async wrapper add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require ('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './env'
@@ -8,4 +9,13 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running successfully at PORT: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!", err);
+    
+})
 
