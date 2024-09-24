65bd6dc3a9fa32811266b34d06b750e744f74452
Manojgond <manojbgond2001@gmail.com>
2024-09-24 21:17:32 +0530

    File upload utilities added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,4 +56,38 @@ userSchema.pre("save", async function (next) {
     next()
 })
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessTokens = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshTokens = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 export const User = mongoose.model("User", userSchema)
